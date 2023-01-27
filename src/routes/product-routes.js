551c0ad75a81dc5c920404d57e41d7f3b0a7a5f3
aurel <aurel@DESKTOP-D75QB98.local>
2023-01-27 14:09:33 -0300

    Update .gitignore, server.js, and 6 more files...

---
+++ src/routes/product-routes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/product-controller');
+
+router.post('/', controller.post);
+
+router.put('/:id', controller.put);
+
+
+module.exports = router;
