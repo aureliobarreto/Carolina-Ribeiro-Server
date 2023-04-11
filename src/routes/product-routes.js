69c04593d5657c0dd611f0ee535c7bdcaa5274ca
Aurelio <aurelionadjabarreto@gmail.com>
2023-04-11 10:21:56 -0300

    Update product-routes.js

--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -4,8 +4,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
- router.post('/', controller.post);
+router.post('/', controller.post);
 router.put('/:id', controller.put);
-
+router.get('/', controller.get);
 
 module.exports = router;
