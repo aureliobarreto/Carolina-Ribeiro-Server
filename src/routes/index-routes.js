a88d09fa48d6cf388453e489e9aca8860ff27156
Aurelio <aurelionadjabarreto@gmail.com>
2023-04-11 10:52:35 -0300

    Update index-routes.js

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -4,11 +4,23 @@ const express = require('express');
 const router = express.Router();
 
  router.get('/', (req ,res, next) => {
-    console.log(req)
-    res.status(200).send({
+    console.log('PARAMS: ', req.query)
+    if(req.query.opt == '01'){
+        res.status(200).send({
+            message: "opt1 !!!!"
+        });
+    }else{
+        res.status(200).send({
         title: "Carolina Ribeiro Grifes",
         version: "0.0.1"
-    }); 
+        }); 
+    }
+    
+
+    /*res.status(200).send({
+        title: "Carolina Ribeiro Grifes",
+        version: "0.0.1"
+    }); */
 });
 
 
