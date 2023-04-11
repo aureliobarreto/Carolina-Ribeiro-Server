6190efc2a81535b98804ebad60bc146dd90ab5da
Aurelio <aurelionadjabarreto@gmail.com>
2023-04-11 10:11:53 -0300

    Update product-controller.js

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const product = require('../models/product');
 const Product = mongoose.model('Product');
 
 exports.post = (req, res, next) => {
     console.log(req.body);
+    prod = new Product(req.body);
+    prod.save().then(x => {
+        res.status(200).send({
+            message: 'Produto cadastrado com sucesso'
+        })
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Erro ao cadastrar',
+            data: e
+        })
+    })
     /*
     var product = new Product(req.body);
     product.save().then(r => {
